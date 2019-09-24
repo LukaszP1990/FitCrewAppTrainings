fda06cc0da5e258a49e09521c2699d31d92f7ef2
Łukasz <lukaszptaszek@interia.com>
2019-09-24 14:48:41 +0200

    trainings ws

---
+++ src/main/java/com/fitcrew/FitCrewAppTrainings/FitCrewAppTrainingsApplication.java
@@ -0,0 +1,13 @@
+package com.fitcrew.FitCrewAppTrainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCrewAppTrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitCrewAppTrainingsApplication.class, args);
+	}
+
+}
